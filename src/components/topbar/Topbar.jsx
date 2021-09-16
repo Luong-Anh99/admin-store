17db6ac108d7a5d0c3e058588bacea5da66dcb4f
Luong-Anh99 <khimalata@gmail.com>
2021-09-16 15:46:57 +0700

    Làm xong phần topbar vs sidebar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Settings, Language } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">LA admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconsContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconsContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconsContainer">
+            <Settings />
+           
+          </div>
+          <img src="https://i.pinimg.com/474x/a9/e6/85/a9e685315c3761f64bf490264c3e1421.jpg" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
