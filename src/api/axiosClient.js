245eb823a3aaa3e0944b161e0dd87a39294e2ba8
anh.luong1 <anh.luong1@mservice.com.vn>
2022-07-07 15:21:07 +0700

    fix env

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,26 +2,17 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import queryString from "querystring";
 import Cookies from "js-cookie";
-const isDevelop =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+// const isDevelop =
+//   !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
-const axiosClient = axios.create(
-  isDevelop
-    ? {
-        baseURL: "https://shoe-shop-demo.herokuapp.com",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      }
-    : {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        paramsSerializer: (params) => queryString.stringify(params),
-        withCredentials: true,
-      }
-);
+const axiosClient = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  //baseURL: "https://shoe-shop-demo.herokuapp.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
 
 axiosClient.interceptors.request.use(async (config) => {
   return config;
@@ -44,3 +35,24 @@ axiosClient.interceptors.response.use(
 );
 
 export default axiosClient;
+
+// const isDevelop =
+//   !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
+// const axiosClient = axios.create(
+//   isDevelop
+//     ? {
+//         baseURL: "https://shoe-shop-demo.herokuapp.com",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         withCredentials: true,
+//       }
+//     : {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         paramsSerializer: (params) => queryString.stringify(params),
+//         withCredentials: true,
+//       }
+// );
